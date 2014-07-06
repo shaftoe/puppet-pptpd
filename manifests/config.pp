36ae4b1e5ff630afd46a30222e204a5f02ba38a8
Tron Thongsringklee <tron@bashton.com>
2014-07-06 20:37:57 +0100

    Add ability to push DNS to clients

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,4 +12,10 @@ class pptpd::config () inherits pptpd {
     content => template('pptpd/pptpd.conf.erb')
   }
 
+  file { $pptpd::option:
+    ensure  => $pptpd::ensure,
+    owner   => 'root',
+    mode    => '0440',
+    content => template('pptpd/pptpd-options.erb')
+  }
 }
